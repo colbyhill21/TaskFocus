a7a4bd57963f06b93c77ffc23aa917d6af0ba264
colbyhill21 <acehill21@ymail.com>
2020-02-20 09:03:42 -0500

    Addded timer functionality as well as start / pause

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,18 @@ export class AppComponent {
     shortBreak = false;
     longBreak = false;
     startStopText = "Start";
-    time = "25:00";
+    numFocus = 0;
+
+    focusLength = 1;
+    shortBreakLength = 5;
+    longBreakLength = 20;
+
+    currentLength = this.focusLength; // the selector based on the current segment (focus, sb, lb)
+    seconds = 0;
+    time = "" + this.currentLength + ":00";
+    timeLeft: number = this.focusLength * 60;
+    interval;
+
 
     focusSelected() {
         this.focus = true;
@@ -30,15 +41,47 @@ export class AppComponent {
         console.log("Hit");
     }
     startStopPressed() {
-        if(this.startStopText == "Start") {
-            this.startStopText = "Stop"
+        if (this.startStopText == "Start") {
+            this.startStopText = "Stop";
+            this.startTimer();
         }
         else {
-            this.startStopText = "Start"
+            this.startStopText = "Start";
+            this.pauseTimer();
         }
     }
     resetPressed() {
-        console.log("will reset");
+        this.timeLeft = this.currentLength * 60;
+        clearInterval(this.interval);
+        this.startStopText = "Start";
+        this.time = "" + this.currentLength + ":00";
+    }
+
+    startTimer() {
+        this.interval = setInterval(() => {
+            if (this.timeLeft > 0) {
+                this.timeLeft--;
+                this.time = "" + (Math.trunc(this.timeLeft / 60)) + ":"
+                let s =  Math.trunc(this.timeLeft % 60);
+                if(s < 10) {
+                    this.time += "0" + s;
+                }
+                else {
+                    this.time += s;
+                }
+            } else {
+                this.numFocus += 1;
+                this.startBreak();
+            }
+        }, 1000)
+    }
+
+    pauseTimer() {
+        clearInterval(this.interval);
+    }
+
+    startBreak() {
+        
     }
 
 }
