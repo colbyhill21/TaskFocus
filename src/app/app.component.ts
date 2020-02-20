3ff8f9dbd15eaf630b4671b909ed74ab06b96c66
colbyhill21 <acehill21@ymail.com>
2020-02-19 20:59:18 -0500

    Setup all UI Components for the app and configured most buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TaskFocus';
+    title = 'TaskFocus';
+    focus = true;
+    shortBreak = false;
+    longBreak = false;
+    startStopText = "Start";
+
+    focusSelected() {
+        this.focus = true;
+        this.shortBreak = false;
+        this.longBreak = false;
+    }
+    shortBreakSelected() {
+        this.focus = false;
+        this.shortBreak = true;
+        this.longBreak = false;
+    }
+    longBreakSelected() {
+        this.focus = false;
+        this.shortBreak = false;
+        this.longBreak = true;
+        console.log("Hit");
+    }
+
 }
