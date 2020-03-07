0a887f7dd2370fd0d8f1b242b1c5ee14b17c7964
colbyhill21 <acehill21@ymail.com>
2020-03-07 10:48:05 -0500

    Added another test to the stats modal

--- src/app/stats-modal/stats-modal.component.spec.ts
+++ src/app/stats-modal/stats-modal.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogRef } from '@angular/material/dialog';
 import { StatsModalComponent } from './stats-modal.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser'; 
 
 describe('StatsModalComponent', () => {
   let component: StatsModalComponent;
   let fixture: ComponentFixture<StatsModalComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,9 +28,15 @@ describe('StatsModalComponent', () => {
         numLongBreak: 0
     };
     fixture.detectChanges();
+    de = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have an H1 Title of `Focus Statistics`', () => {
+    expect(de.query(By.css('h1')).nativeElement.innerText).toBe('Focus Statistics');
+  });
+
 });
